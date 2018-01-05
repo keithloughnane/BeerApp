a069227542470d3e4ec12b7b3176fe558dd8aa13
keith.loughnane <keith.loughnane@gmail.com>
2018-01-05 23:57:05 +0200

    main screen basic functionality working.

--- app/src/main/java/com/keithloughnane/beer/beerapp/BeerViewHolder.java
+++ app/src/main/java/com/keithloughnane/beer/beerapp/BeerViewHolder.java
@@ -4,10 +4,12 @@ import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.CollapsibleActionView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.keithloughnane.beer.beerapp.activities.BeerModel;
 import com.keithloughnane.beer.beerapp.data.Beer;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -22,14 +24,45 @@ public class BeerViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.title)
     TextView title;
 
+    @BindView(R.id.beer_image)
+    ImageView beerImage;
+
+    @BindView(R.id.tag_line)
+    TextView tagLine;
+
+    @BindView(R.id.abv)
+    TextView abv;
+
+    @BindView(R.id.ibu)
+    TextView ibu;
+
+    @BindView(R.id.ebc)
+    TextView ebc;
+
+    @BindView(R.id.srm)
+    TextView srm;
+
+    @BindView(R.id.ph)
+    TextView ph;
+
     public BeerViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
     }
 
     public void bind(Beer beer) {
-
-        //((TextView)itemView.findViewById(R.id.title)).setText(beer.name);
         title.setText(beer.name);
+        tagLine.setText(beer.tagline);
+        abv.setText("" + beer.abv);
+        ibu.setText("" + beer.ibu);
+        srm.setText("" + beer.srm);
+        ph.setText("" + beer.ph);
+
+        Picasso
+                .with(itemView.getContext())
+                .load(beer.image_url)
+                .fit()
+                .centerInside()
+                .into(beerImage);
     }
 }
