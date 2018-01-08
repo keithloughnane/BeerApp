70816819f76303adf2b3c4c565cfacc15cfdb9bc
keith.loughnane <keith.loughnane@gmail.com>
2018-01-08 22:52:33 +0200

    minor, prevent warning

--- app/src/main/java/com/keithloughnane/beer/beerapp/dataAccess/local/BeerDatabase.java
+++ app/src/main/java/com/keithloughnane/beer/beerapp/dataAccess/local/BeerDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.RoomDatabase;
 
 import com.keithloughnane.beer.beerapp.data.Beer;
 
-@Database(entities = {Beer.class}, version = 1)
+@Database(entities = {Beer.class}, version = 1, exportSchema = false)
 public abstract class BeerDatabase extends RoomDatabase {
     public abstract BeerStorage beerStorage();
 }
